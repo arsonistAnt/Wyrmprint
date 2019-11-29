b98c6e93bfc6bd660cee164418f58e994b9cf5a4
arsonistAnt <anthonynguyen194@gmail.com>
2019-11-29 13:16:24 -0800

    Thumbnail ui without weights

--- app/src/main/java/com/example/wyrmprint/ui/browse/viewholder/ThumbnailItemView.kt
+++ app/src/main/java/com/example/wyrmprint/ui/browse/viewholder/ThumbnailItemView.kt
@@ -2,6 +2,7 @@ package com.example.wyrmprint.ui.browse.viewholder
 
 import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
 import coil.api.load
 import com.example.wyrmprint.R
 import com.example.wyrmprint.data.model.ComicThumbnailData
@@ -19,13 +20,15 @@ open class ThumbnailItemView(private val thumbnailData: ComicThumbnailData?) :
 
     class ThumbnailCardHolder(view: View) : FastAdapter.ViewHolder<ThumbnailItemView>(view) {
         private val thumbnailImage: ImageView? = view.findViewById(R.id.thumbnail_item)
+        private val thumnailTitle: TextView? = view.findViewById(R.id.thumbnail_title)
 
-        override fun bindView(itemView: ThumbnailItemView, payloads: MutableList<Any>) {
-            thumbnailImage?.load(itemView.thumbnailData?.thumbnailLarge) {
+        override fun bindView(item: ThumbnailItemView, payloads: MutableList<Any>) {
+            thumbnailImage?.load(item.thumbnailData?.thumbnailLarge) {
                 placeholder(R.drawable.loading_placeholder)
             }
+            thumnailTitle?.text = item.thumbnailData?.title
         }
 
-        override fun unbindView(itemView: ThumbnailItemView) {}
+        override fun unbindView(item: ThumbnailItemView) {}
     }
 }
