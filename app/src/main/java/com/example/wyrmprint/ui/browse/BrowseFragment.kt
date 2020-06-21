48798df13db0e5565b087cdab02a6f043b6ad774
Anthony <arsonistant@gmail.com>
2020-06-21 02:40:01 -0700

    Fix long click selection in the browse section after swipe refresh
    Long pressing on the comic strip after swiping to refresh would not work, this issue has been solved by using the SelectionExtension class provided by the fast adapter.

--- app/src/main/java/com/example/wyrmprint/ui/browse/BrowseFragment.kt
+++ app/src/main/java/com/example/wyrmprint/ui/browse/BrowseFragment.kt
@@ -30,6 +30,8 @@ import com.mikepenz.fastadapter.IItem
 import com.mikepenz.fastadapter.adapters.GenericItemAdapter
 import com.mikepenz.fastadapter.paged.ExperimentalPagedSupport
 import com.mikepenz.fastadapter.paged.GenericPagedModelAdapter
+import com.mikepenz.fastadapter.select.SelectExtension
+import com.mikepenz.fastadapter.select.getSelectExtension
 import com.mikepenz.fastadapter.ui.items.ProgressItem
 
 /**
@@ -41,6 +43,7 @@ class BrowseFragment : Fragment() {
     private lateinit var browsePageItemAdapter: GenericPagedModelAdapter<ThumbnailData>
     private lateinit var footerItemAdapter: GenericItemAdapter
     private lateinit var fastAdapter: GenericFastAdapter
+    private lateinit var selectionExt: SelectExtension<*>
     private val browserViewModel: BrowserViewModel by viewModel { injector.browserViewModel }
 
     // Default span count for different orientations.
@@ -125,9 +128,8 @@ class BrowseFragment : Fragment() {
         }
         onLongClickListener = { _, adapter, item, pos ->
             adapter.fastAdapter?.let {
-                toggleFavorites(item as ThumbnailItemView)
                 // Update the item view appearance when the favorites state has changed.
-                it.notifyAdapterItemChanged(pos)
+                toggleFavorites(item as ThumbnailItemView, pos)
             }
             true
         }
@@ -142,7 +144,7 @@ class BrowseFragment : Fragment() {
     private fun initBrowserRecyclerView(browser: RecyclerView) {
         // Create a generic fast adapter that will take in a paged model adapter and a generic item adapter.
         fastAdapter = createFastAdapter()
-
+        selectionExt = fastAdapter.getSelectExtension()
         // Init grid layout manager & set span size lookup for ProgressItem.
         val gridLayoutManager = getGridLayoutManager(fastAdapter)
 
@@ -166,7 +168,6 @@ class BrowseFragment : Fragment() {
                 Observer { thumbnailPagedList ->
                     updateThumbnailPagedList(thumbnailPagedList)
                 })
-
             thumbnailNetworkStatus.observe(
                 viewLifecycleOwner,
                 Observer {
@@ -304,10 +305,10 @@ class BrowseFragment : Fragment() {
      *
      * @param item the [ThumbnailItemView] that contains a [ThumbnailData] to save.
      */
-    private fun saveToFavorites(item: ThumbnailItemView) {
+    private fun saveToFavorites(item: ThumbnailItemView, pos: Int) {
         item.model.apply {
-            item.isSelected = true
             isFavorite = true
+            fastAdapter.getSelectExtension().select(pos)
             browserViewModel.addToFavorites(this)
         }
     }
@@ -317,10 +318,10 @@ class BrowseFragment : Fragment() {
      *
      * @param item the [ThumbnailItemView] that contains a [ThumbnailData] to be removed.
      */
-    private fun removeFavorites(item: ThumbnailItemView) {
+    private fun removeFavorites(item: ThumbnailItemView, pos: Int) {
         item.model.apply {
-            item.isSelected = false
             isFavorite = false
+            fastAdapter.getSelectExtension().deselect(pos)
             browserViewModel.removeFromFavorites(this)
         }
     }
@@ -336,13 +337,17 @@ class BrowseFragment : Fragment() {
 
     /**
      * Toggle favorites for a particular item view.
+     *
+     * @param item the [ThumbnailItemView] that contains the comic data of interest.
+     * @param pos the position of the [item] in the recycler view.
      */
-    private fun toggleFavorites(item: ThumbnailItemView) {
+    private fun toggleFavorites(item: ThumbnailItemView, pos: Int) {
         item.model.apply {
-            if (!isFavorite)
-                saveToFavorites(item)
-            else
-                removeFavorites(item)
+            if (!isFavorite) {
+                saveToFavorites(item, pos)
+            } else {
+                removeFavorites(item, pos)
+            }
         }
     }
 }
